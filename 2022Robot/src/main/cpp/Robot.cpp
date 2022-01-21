584f93debbe8a6ba8b0a417d4cd50d27ed315085
AyantuT <ayantut2022@gmail.com>
2022-01-20 17:26:16 -0800

    Wrote the drive code to drive forward and back

---
+++ 2022Robot/src/main/cpp/Robot.cpp
@@ -0,0 +1,134 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+// O_o
+#include "Robot.h"
+#include <fmt/core.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <ctre/Phoenix.h>
+#include <frc/Timer.h>
+#include <frc/TimedRobot.h>
+//#include <PowerDistribution.h>
+#include <frc/Joystick.h>
+#include <frc/ADXRS450_Gyro.h>
+#include <frc/Solenoid.h>
+// :^)   :|
+
+//Declarations
+
+//Variables
+
+//Joysticks
+frc::Joystick Joystick, Wheel, Xbox;
+
+
+//Motors
+TalonFX FrontLeftMotor {0};
+TalonFX MiddleLeftMotor {1};
+TalonFX BackLeftMotor {2};
+TalonFX FrontRightMotor {3};
+TalonFX MiddleRightMotor {4};
+TalonFX BackRightMotor {5};
+
+//Power Distribution Panel
+
+
+//Set up motors to drive
+void LeftMotorDrive (double speed) {
+  FrontLeftMotor.Set(ControlMode::PercentOutput, speed);
+  MiddleLeftMotor.Set(ControlMode::PercentOutput, speed);
+  BackLeftMotor.Set(ControlMode::PercentOutput, speed);
+}
+void RightMotorDrive (double speed) { 
+  FrontRightMotor.Set(ControlMode::PercentOutput, speed);
+  MiddleRightMotor.Set(ControlMode::PercentOutput, speed);
+  BackRightMotor.Set(ControlMode::PercentOutput, speed);
+}
+
+// :D    :]
+
+//Initializing robot & variables
+void Robot::RobotInit() {
+  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
+  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
+  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+
+
+}
+
+/**
+ * This function is called every robot packet, no matter the mode. Use
+ * this for items like diagnostics that you want ran during disabled,
+ * autonomous, teleoperated and test.
+ *
+ * <p> This runs after the mode specific periodic functions, but before
+ * LiveWindow and SmartDashboard integrated updating.
+ */
+void Robot::RobotPeriodic() {
+  double JoyY = Joystick.GetRawAxis();
+  
+  //If joystick is pushed forward
+  if (JoyY > 0.1) {
+    LeftMotorDrive(JoyY);
+    RightMotorDrive(JoyY);
+  }
+  if (JoyY < -0.1){
+    LeftMotorDrive(-JoyY);
+    RightMotorDrive(-JoyY);
+  }
+
+
+}
+
+/**
+ * This autonomous (alon)g with the chooser code above) shows how to select
+ * between different autonomous modes using the dashboard. The sendable chooser
+ * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+ * remove all of the chooser code and uncomment the GetString line to get the
+ * auto name from the text box below the Gyro.
+ *
+ * You can add additional auto modes by adding additional comparisons to the
+ * if-else structure below with additional strings. If using the SendableChooser
+ * make sure to add them to the chooser code above as well.
+ */
+void Robot::AutonomousInit() {
+  m_autoSelected = m_chooser.GetSelected();
+  // m_autoSelected = SmartDashboard::GetString("Auto Selector",
+  //     kAutoNameDefault);
+  fmt::print("Auto selected: {}\n", m_autoSelected);
+
+  if (m_autoSelected == kAutoNameCustom) {
+    // Custom Auto goes here
+  } else {
+    // Default Auto goes here
+  }
+}
+
+void Robot::AutonomousPeriodic() {
+  if (m_autoSelected == kAutoNameCustom) {
+    // Custom Auto goes here
+  } else {
+    // Default Auto goes here
+  }
+}
+
+void Robot::TeleopInit() {}
+
+void Robot::TeleopPeriodic() {}
+
+void Robot::DisabledInit() {}
+
+void Robot::DisabledPeriodic() {}
+
+void Robot::TestInit() {}
+
+void Robot::TestPeriodic() {}
+
+#ifndef RUNNING_FRC_TESTS
+int main() {
+  return frc::StartRobot<Robot>();
+}
+#endif
